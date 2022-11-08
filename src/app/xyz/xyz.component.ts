2bfe0503d6009144f895fd6df0277dd096919401
ViliusVed <vilius.vedegys@gmail.com>
2022-11-09 00:59:13 +0200

    login forma  design

--- src/app/xyz/xyz.component.ts
+++ src/app/xyz/xyz.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class XyzComponent implements OnInit {
 
+  type: string = "password";
+  isText: boolean = false;
+  eyeIcon: string = "fa-eye-slash";
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  hideShowPass() {
+    this.isText = !this.isText;
+    this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
+    this.isText ? this.type = "text" : this.type = "password";
   }
 
 }
